8fe85c517faf30718f96a7b40edc387f258dc0cc
Marie Jossé <84919248+Marie59@users.noreply.github.com>
2023-08-17 16:44:03 +0200

    change java command

--- scoop3-argo/scoop_argo.sh
+++ scoop3-argo/scoop_argo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCOOP3_JAR=./scoop_argo/scoop3-argo-controller-1.40-executable.jar
 PROPERTIES=./properties
-java -Xms1024m -classpath ${PROPERTIES}:${SCOOP3_JAR} fr.ifremer.scoop3.argo.application.ArgoApplication
+./java -Xms1024m -classpath ${PROPERTIES}:${SCOOP3_JAR} fr.ifremer.scoop3.argo.application.ArgoApplication
